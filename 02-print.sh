2861883e6ab7787294a5162148465b8b3bbcc8c0
hari kishan <kishanviit13@gmail.com>
2022-03-15 15:15:11 +0530

    esc secquence in echo command

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,10 @@ echo next time we need to try
 echo -e "\e[36m future focus\e[0m"
 echo "next line"
 
+echo -e "LINE1\nline2"
+
+echo -e " line1\n\nline2"
+
+echo -e "word1\tword2"
+
+echo -e "word1\t\tword2"
