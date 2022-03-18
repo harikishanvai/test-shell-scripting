cdf27d322e1fba7842945d1868ee30951a18e8ed
hari kishan <kishanviit13@gmail.com>
2022-03-18 16:53:35 +0530

    in conditins if command

--- 06-condition.sh
+++ 06-condition.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 a="abc"
-if[ "$a" == "abc" ]
-then
+if[ "$a" == "abc" ]; then
   echo both are equal
 fi
 
-if[ "$a" != "abc" ]
-then
+if[ "$a" != "abc" ]; then
   echo "both are not equal"
 fi
 
-if[ -z "$b" ] ; then
-  echo "b variable is empty"
+if[ -z "$b" ]; then
+  echo b variable is empty
