ec4be987f57386af2f4e01926f4f388f3364ba04
hari kishan <kishanviit13@gmail.com>
2022-03-18 22:17:21 +0530

    functions command

--- 07-functions.sh
+++ 07-functions.sh
@@ -2,5 +2,20 @@ Print_Message() {
   echo Hello
   echo Good Morning
   echo Welcome to Devops Traning
+  echo "First Argument in Function = $1"
+  a=20
+  echo "value of a = $a"
+  b=20
 }
-Print_Message
+STAT() {
+  echo Hello
+  return 1
+  echo Bye
+}
+a=10
+Print_Message Devops
+
+echo "First Arugument is Mian Script = $1"
+echo "Value of b = $b"
+STAT
+echo exit ststus of function STAT =$?
