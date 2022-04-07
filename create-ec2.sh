93cff828c3538a3ecbec07d8010a0cff67277cf5
hari kishan <kishanviit13@gmail.com>
2022-04-07 21:08:38 +0530

    create ec2 instance script

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,4 +4,4 @@ AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pr
 
 echo $AMI_ID
 
-aws ec2 run-instances --image-id${AMI_ID} --instance-type t2.micro
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
