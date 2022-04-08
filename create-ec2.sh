803d50b7186e5c8fbd523c65023e829411ce1f40
hari kishan <kishanviit13@gmail.com>
2022-04-08 13:23:28 +0530

    create ec2 instance script

--- create-ec2.sh
+++ create-ec2.sh
@@ -10,11 +10,11 @@ ZONE_ID="Z040913627YDP2ZIGZP4C"
 
 
 AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-public | jq '.SecurityGroups[]'.GroupId | sed -e 's/"//g')
+SGID=$( aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-public | jq '.SecurityGroups[]'.GroupId | sed -e 's/"//g')
 
 echo $AMI_ID
 
-PRIVATE_IP=$(aws ec2 run-instances \
+PRIVATE_IP=$( aws ec2 run-instances \
  --image-id ${AMI_ID} \
  --instance-type t2.micro \
  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
