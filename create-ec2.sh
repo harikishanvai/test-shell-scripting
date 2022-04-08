85de29ea82edd689120ce9d9981c0b3df77818c3
hari kishan <kishanviit13@gmail.com>
2022-04-08 12:28:30 +0530

    create ec2 instance script

--- create-ec2.sh
+++ create-ec2.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ]; then
 fi
 
 COMPONENT=$1
+ZONE_ID="Z040913627YDP2ZIGZP4C"
 
 
 AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
@@ -13,10 +14,13 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-a
 
 echo $AMI_ID
 
-aws ec2 run-instances \
+PRIVATE_IP=$(aws ec2 run-instances \
  --image-id ${AMI_ID} \
  --instance-type t2.micro \
  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
   --security-group-ids ${SGID} \
-   | jq
+   | jq 'Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+sed -e "s/IPADDRESS/${PRIVATE_IP/}" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
